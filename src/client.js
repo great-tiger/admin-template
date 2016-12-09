91a23bd887b18a38d5c094cacea337bfc2e71371
chenlihu <892873387@qq.com>
2016-12-09 11:19:39 +0800

    add ContentWrapper

--- src/client.js
+++ src/client.js
@@ -7,12 +7,14 @@ import './styles/bootstrap.css';
 import './styles/AdminLTE.css';
 import './styles/skins/skin-blue.css';
 import MainSidebar from './components/MainSidebar';
+import ContentWrapper from './components/ContentWrapper';
 
 ReactDOM.render(
   <AppContainer>
     <div className="skin-blue">
       <Header />
       <MainSidebar />
+      <ContentWrapper />
     </div>
   </AppContainer>,
   document.getElementById('app')
---
+++ src/components/ContentWrapper.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class ContentWrapper extends React.Component {
+	render(){
+		return (
+			<div className="content-wrapper">
+			    This is content wrapper......
+			</div>
+		);
+	}
+}
